d660a1be813849e2ab9b0bbf27626957718e095f
PJay2004 <jayasinghepoorna@gmail.com>
2024-04-24 02:43:31 +0530

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='Header'>
+            <h1>Header</h1>
+        </div>
+
+
+    )
+}
+
+export default Header
